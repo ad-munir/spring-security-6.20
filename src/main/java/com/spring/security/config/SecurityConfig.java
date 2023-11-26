228c021279bdaa7d7024ce16427c011410eaac8b
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-11-26 21:21:47 +0100

    initial

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -6,19 +6,20 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(auth ->
-//            auth.requestMatchers("/api/home/**").permitAll()
-//                    .requestMatchers("/api/profile/**").hasAnyRole("ADMIN", "USER")
-//                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
-//        )
-//        .formLogin(Customizer.withDefaults())
-//        .httpBasic(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+            auth.requestMatchers("/api/home/**").permitAll()
+                    .requestMatchers("/api/profile/**").hasAnyRole("ADMIN", "USER")
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+//                    .anyRequest().authenticated()
+                )
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
